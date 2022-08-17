f0db3506a20f59c9fe0f900e1c03aaa1877c8fb9
arslan-mustafa <111464120+arslan-mustafa@users.noreply.github.com>
2022-08-18 01:55:28 +0300

    Updated on 18.08.2022
    add pieces and pawn classes

--- chess.cpp
+++ chess.cpp
@@ -7,70 +7,83 @@
 
 using namespace std;
 
-/* USED RESOURCES
-https://www.geeksforgeeks.org/array-of-objects-in-c-with-examples/
-*/
-
-
 //Path of info txt
 string path = "/home/ubuntu/Desktop/Codes/Applicant Assesment Test_4_EK_AtFil/board1.txt";
 
-//Define a queue that holds to all of the threat conditions
-queue<string> threat;
 
 //Define Board for implement the every pieces on an array.
-//Dimensions are y_size, x_size,
 string Board[8][8];
+
+//Initialize an array that will keep the threatened stones to be used in the score calculation
+string threatened_pieces[8][8];
 
 /*----------------------------------------------------------------------*/
 /*                      Define the Pieces classes                       */
 /*----------------------------------------------------------------------*/
 
+//Define pieces Abstract class to reach all of the Pieces from here
 class Pieces
 {
 	public:
-		char color;
+	//Define name and color of the pieces
 		char name;
-		virtual void isThread();
-		void setColor(char c)
+		char color;
+		int x,y;
+	//assign constructor
+		Pieces(char n, char c, int pos_x, int pos_y)
 		{
-			color = c;
+			n= name;
+			c = color;
+			x = pos_x;
+			y = pos_y;
 		}
-		void setName(char n)
-		{
-			name = n;
-		}
+	//Pure virtual function
+		virtual void isThread() = 0;
 };
 
-class pawn : public Pieces
+class pawn
 {		// The class
-	public:          
-		char color;
-		char name;
-		int x_position;
-		int y_position;
-
-		pawn(int x, int y, char c)
-		{ // Constructor with parameters
-		color = c;
-		x_position = x;
-		y_position = y;
-		}
-		void isThread(int x, int y, char c)
+	public:
+		char pawn_color;
+		char pawn_name;
+		int x,y;
+		pawn(char col, char name, int pos_x, int pos_y)
 		{
-			c = color;
+			pawn_color = col;
+			pawn_name = name;
+			x = pos_x;
+			y = pos_y;
+		}
+		void isThread(void)
+		{
+			char c = pawn_color;
+		//Black side
 			if (c == 's' && Board[x + 1][y + 1] != "--" && Board[x - 1][y + 1] != "--")
 			{
-				threat.push(Board[x + 1][y + 1]);
-				threat.push(Board[x - 1][y + 1]);
+				threatened_pieces[x+1][y+1] = Board[x + 1][y + 1];
+				threatened_pieces[x+1][y+1] = Board[x - 1][y + 1];
 			}
-			else if (c == 's' && Board[x + 1][y + 1] != "--")
+			else if (c == 's' && Board[x+1][y+1] != "--")
 			{
-				threat.push(Board[x + 1][y + 1]);
+				threatened_pieces[x+1][y+1] = Board[x+1][y+1];
 			}
-			else if (c == 's' && Board[x - 1][y + 1] != "--")
+			else if (c == 's' && Board[x-1][y+1] != "--")
 			{
-				threat.push(Board[x - 1][y + 1]);
+				threatened_pieces[x-1][y+1] = Board[x-1][y+1];
+			}
+		//white side
+			if (c == 'b' && Board[x + 1][y - 1] != "--" && Board[x - 1][y - 1] != "--")
+			{
+				threatened_pieces[x+1][y-1] = Board[x - 1][y - 1];
+				threatened_pieces[x+1][y-1] = Board[x - 1][y - 1];
+			}
+			else if (c == 'b' && Board[x-1][y-1] != "--")
+			{
+				threatened_pieces[x+1][y-1] = Board[x+1][y-1];
+			}
+			else if (c == 'b' && Board[x-1][y-1] != "--")
+			{
+				threatened_pieces[x-1][y-1] = Board[x-1][y-1];
 			}
 
 		}
@@ -115,6 +128,16 @@ for (int y = 0; y < 8; y++)
 	}
 }
 cout<<endl;
+
+cout<< endl<<endl<<"print threat Array"<<endl;
+for (int y = 0; y < 8; y++)
+{
+	for (int x = 0; x < 8; x++)
+	{
+		cout<<threatened_pieces[y][x]<<' ';
+	}
+}
+cout<<endl;
 }
 
 void PutThePieces()
@@ -125,12 +148,10 @@ for (int y = 0; y < 8; y++)
 	{
 		if( Board[x][y] == "pb")
 		{
-			pawn p;
-			pawn.setColor('b');
-			pawn.setName('p');
+			pawn('b', 'p', x, y);
 		}
 		else if(Board[x][y] == "ps")
-			
+			pawn('s', 'p', x, y);
 	}
 }
 
